9349375f3014a5869fba9b5ca99e55d5258db0cc
Sammy <samsimss98@gmail.com>
2020-06-29 11:10:18 +0100

    get_top_tracks and get_top_songs return dictionary of values I want instead of entire dictionary returned by spotipy

--- functions/functions.py
+++ functions/functions.py
@@ -15,10 +15,24 @@ def get_user_top_artists(token, range):
     sp = spotipy.Spotify(auth=token)
     response = sp.current_user_top_artists(limit=20, time_range=range)
     print(response)
-    return response
+    list = []
+    for i, item in enumerate(response['items']):
+        dictOfArtists = {
+            "name": item['name'],
+            "popularity": item['popularity']
+        }
+        list.append(dictOfArtists)
+    return list
 
 def get_user_top_tracks(token, range):
     sp = spotipy.Spotify(auth=token)
     response = sp.current_user_top_tracks(limit=20, time_range=range)
     print(response)
-    return response
+    list = []
+    for i, item in enumerate(response['items']):
+        dictOfArtists = {
+            "name": item['name'],
+            "artist_name": item['artists'][0]['name']
+        }
+        list.append(dictOfArtists)
+    return list
